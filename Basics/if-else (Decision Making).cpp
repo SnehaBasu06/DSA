08876932855d40a4c953741c875e60adc34d7a04
SnehaBasu06 <94731983+SnehaBasu06@users.noreply.github.com>
2024-04-05 14:43:09 +0530

    Create if-else (Decision Making).cpp

---
+++ Basics/if-else (Decision Making).cpp
@@ -0,0 +1,13 @@
+string compareIfElse(int a, int b) {
+	// Write your code here
+	string res; 
+	if(a>b){
+		res="greater";
+	}
+	else if(a<b){
+		res="smaller";
+	}
+	else{
+		res="equal";
+	}
+return res;}
